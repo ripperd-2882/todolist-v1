89afd835976c6c716c0b0dafc035b153f122b295
ripperd-2882 <ripperd2882@gmail.com>
2023-04-14 02:03:11 +0530

    added work about heades & footer pages

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-let items=["Buy Food","Cook Food","Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -33,13 +34,37 @@ app.get("/", function (req, res) {
 
     // day=weekday[currentDay];
 
-    res.render("list", { kindOfDay: day, newListItems: items });
+    res.render("list", { listTitle: day, newListItems: items });
 })
 
 app.post("/", function (req, res) {
+
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+
+        let item = req.body.newItem;
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems })
+});
+
+app.post("/work", function (req, res) {
+    let workItem = req.body.newItem;
+    workItems.push(workItem);
+    res.redirect("/work");
+});
+
+app.get("/about", function (req, res) {
+    res.render("about")
 });
 
 app.listen(3000, function () {
