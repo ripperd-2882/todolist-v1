d5c9e214c3d8e5e1894db1d6901db07e5e5576e6
ripperd-2882 <ripperd2882@gmail.com>
2023-04-14 01:04:43 +0530

    Added feature to add an element in the todo list

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
-const express=require("express");
-const bodyParser=require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+var items=["Buy Food","Cook Food","Eat Food"];
+
 app.set('view engine', 'ejs');
 
-app.get("/",function (req,res) {
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    var today=new Date();
-    var currentDay=today.getDay();
-    var day="";
-    var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    
+app.get("/", function (req, res) {
+
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+    // var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
     // if (currentDay===6 || currentDay===0){
     //     day=weekday[currentDay];
     // } else {
     //     day="Weekday";
     // }
 
-    day=weekday[currentDay];
+    // day=weekday[currentDay];
 
-    res.render("list",{kindOfDay:day});
+    res.render("list", { kindOfDay: day, newListItems: items });
 })
 
-app.listen(3000,function () {
-    console.log("Server started at port 3000");    
+app.post("/", function (req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
+
+app.listen(3000, function () {
+    console.log("Server started at port 3000");
 })
